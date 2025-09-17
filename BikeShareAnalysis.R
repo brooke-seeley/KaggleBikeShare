40a5f7b220a6cdc6af5dad85e6860ff0cd78eb16
brooke-seeley <brookeseeleyaz@gmail.com>
2025-09-17 11:37:34 -0600

    Tuning parameters

--- BikeShareAnalysis.R
+++ BikeShareAnalysis.R
@@ -27,43 +27,49 @@ bike_recipe <- recipe(count~., data=trainData) %>%
   step_normalize(all_numeric_predictors()) %>%
   step_rm(datetime)
 
-## Create Penalized Regression Model
+## Create Penalized Regression Model for Tuning
 
-### Combo 1: Penalty 0 and Mixture 0
-
-preg_model <- linear_reg(penalty=0, mixture=0) %>%
+preg_model <- linear_reg(penalty=tune(),
+                         mixture=tune()) %>% 
   set_engine("glmnet")
-
-### Combo 2: Penalty 1 and Mixture 0.5
-
-#preg_model <- linear_reg(penalty=1, mixture=0.5) %>%
-  #set_engine("glmnet")
-
-### Combo 3: Penalty 1 and Mixture 1
-
-#preg_model <- linear_reg(penalty=1, mixture=1) %>%
-  #set_engine("glmnet")
-
-### Combo 4: Penalty 5 and Mixture 0.25
-
-#preg_model <- linear_reg(penalty=5, mixture=0.25) %>%
-  #set_engine("glmnet")
-
-### Combo 5: Penalty 2 and Mixture 0.75
-
-#preg_model <- linear_reg(penalty=2, mixture=0.75) %>%
-  #set_engine("glmnet")
 
 ## Create Workflow
 
 bike_workflow <- workflow() %>%
   add_recipe(bike_recipe) %>%
-  add_model(preg_model) %>%
+  add_model(preg_model)
+
+## Tuning
+
+### Grid of Values
+
+grid_of_tuning_params <- grid_regular(penalty(),
+                                      mixture(),
+                                      levels = 5)
+
+### Split Data & Run CV
+
+folds <- vfold_cv(trainData, v = 10, repeats=1)
+CV_results <- bike_workflow %>%
+  tune_grid(resamples=folds,
+            grid=grid_of_tuning_params,
+            metrics=metric_set(rmse, mae, rsq))
+
+### Best Parameters
+
+bestTune <- CV_results %>%
+  select_best(metric="rmse")
+
+## Finalize Workflow & FIt
+
+final_wf <-
+  bike_workflow %>%
+  finalize_workflow(bestTune) %>%
   fit(data=trainData)
 
 ## Run Predictions
 
-bike_predictions <- predict(bike_workflow, new_data = testData) %>%
+bike_predictions <- predict(final_wf, new_data = testData) %>%
   mutate(.pred = exp(.pred))
 
 ## Format the Predictions for Submission to Kaggle
